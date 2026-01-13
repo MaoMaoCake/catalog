7d333bb128fe13694805d041fa0d34ed6d289ef6
Laurent Goderre <laurent.goderre@docker.com>
2026-01-13 13:30:07 -0500

    vault: fix helm entrypoint (#15836)
    [dhi-commit=c98c606be174552fe9e2e310cdead28b1736baa9]

--- image/vault/bin/docker-entrypoint-helm.sh
+++ image/vault/bin/docker-entrypoint-helm.sh
@@ -3,6 +3,11 @@ set -e
 
 ulimit -c 0
 
-set -- vault "$@"
+# Because of the command use in the chart, with the chart command being in the middle of
+# a complex command
+
+if [ "$1" = 'server' ]; then
+	set -- vault "$@"
+fi
 
 exec tini -- "$@"
